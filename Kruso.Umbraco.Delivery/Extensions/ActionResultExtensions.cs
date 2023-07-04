c51619080f32dafbfde600740f16f6450fed5ec8
Ed Soanes <edward@kruso.se>
2023-07-04 14:57:32 +0200

    Useful extension method public

--- Kruso.Umbraco.Delivery/Extensions/ActionResultExtensions.cs
+++ Kruso.Umbraco.Delivery/Extensions/ActionResultExtensions.cs
@@ -1,17 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Kruso.Umbraco.Delivery.Extensions
 {
-    internal static class ActionResultExtensions
+    public static class ActionResultExtensions
     {
         private static JsonSerializerSettings _serializerSettings = new JsonSerializerSettings
         {
@@ -22,7 +16,7 @@ namespace Kruso.Umbraco.Delivery.Extensions
         };
     
 
-        internal static IActionResult ToJsonResult<T>(this T obj, HttpStatusCode statusCode = HttpStatusCode.OK)
+        public static IActionResult ToJsonResult<T>(this T obj, HttpStatusCode statusCode = HttpStatusCode.OK)
             where T : class
         {
             if (obj == null)
